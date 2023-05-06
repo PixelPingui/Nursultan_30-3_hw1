27a7d9e061078ff10647e6f47eaaaf63864146fa
PixelPignui <nkaparov93@gmail.com>
2023-05-06 18:08:44 +0300

    add hw1

---
+++ Nursultan_30-3_hw1.py
@@ -0,0 +1,33 @@
+class SuperHero:
+    people = 'people'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase):
+        self.name = name
+        self.nickname = nickname
+        self.superpower = superpower
+        self.health_points = health_points
+        self.catchphrase = catchphrase
+
+    def print_name(self):
+        print(self.name)
+
+    def double_health_points(self):
+        self.health_points *= 2
+
+    def __str__(self):
+        return f"{self.nickname} has {self.superpower} and {self.health_points} health points"
+
+    def __len__(self):
+        return len(self.catchphrase)
+
+
+hero = SuperHero('Clark Kent', 'Superman', 'super strength', 100, 'Up, up, and away!')
+hero.print_name()  # выводит 'Clark Kent'
+hero.double_health_points()  # удваивает здоровье героя
+print(hero)  # выводит 'Superman has super strength and 200 health points'
+print(len(hero))  # выводит длину коронной фразы героя
+
+
+
+
+
